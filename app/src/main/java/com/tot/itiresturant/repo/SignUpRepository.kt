72c9295049db326fb58740b4ed1db3b6f001d373
lenovo <engineer.marwa@yahoo.com>
2019-10-31 18:42:54 +0200

    ahmed  update 3

--- app/src/main/java/com/tot/itiresturant/repo/SignUpRepository.kt
+++ app/src/main/java/com/tot/itiresturant/repo/SignUpRepository.kt
@@ -1,6 +1,7 @@
 package com.tot.itiresturant.repo
 
 import android.app.Application
+import android.content.Context
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
@@ -10,6 +11,8 @@ class SignUpRepository (application: Application) {
     init {
         mAuth = FirebaseAuth.getInstance()
     }
-    
 
+    fun newUser(emailAddress:String, password:String){
+        mAuth.createUserWithEmailAndPassword(emailAddress, password)
+    }
 }
