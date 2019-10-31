4d70784a393797f2464e51ed09deaed4a1261a89
lenovo <engineer.marwa@yahoo.com>
2019-10-31 18:22:21 +0200

    ahmed  update

--- app/src/main/java/com/tot/itiresturant/view/activity/FirstActivity.kt
+++ app/src/main/java/com/tot/itiresturant/view/activity/FirstActivity.kt
@@ -1,13 +1,23 @@
 package com.tot.itiresturant.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tot.itiresturant.R
+import kotlinx.android.synthetic.main.activity_first.*
 
 class FirstActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        btnSignIn.setOnClickListener{
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
+
+        btnSignup.setOnClickListener{
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
     }
 }
