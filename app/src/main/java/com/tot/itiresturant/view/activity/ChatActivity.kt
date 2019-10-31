1a1da9436ae25552687af326e1c5e3567e910aed
alialfayed1 <alialfayed1@gmail.com>
2019-10-31 15:00:42 +0200

    Update SignIn

---
+++ app/src/main/java/com/tot/itiresturant/view/activity/ChatActivity.kt
@@ -0,0 +1,13 @@
+package com.tot.itiresturant.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tot.itiresturant.R
+
+class ChatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
