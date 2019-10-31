67d8ef2e33ecf588dfbdedd230e0e26d3195229b
AbdElwahabZayed <ammz1997@gmail.com>
2019-10-31 17:49:31 +0200

    file

--- app/src/main/java/com/tot/itiresturant/view/activity/Home.kt
+++ app/src/main/java/com/tot/itiresturant/view/activity/Home.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import com.tot.itiresturant.R
+import com.tot.itiresturant.model.Order
 import com.tot.itiresturant.recyclerDecoration.GridItemDecoration
 import com.tot.itiresturant.view.adapter.HomeAdapter
 import com.tot.itiresturant.viewmodel.OrederViewModel
@@ -29,7 +30,10 @@ lateinit var homeAdapter: HomeAdapter
         homeAdapter= HomeAdapter(this,this)
         home_RecyclerView.adapter=homeAdapter
         orderViewModel.getAllMenuItems().observe(this, Observer {
-
+            homeAdapter.setData(it as ArrayList<Order>)
+            homeAdapter.notifyDataSetChanged()
         })
+        supportActionBar!!.setDefaultDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeAsUpIndicator()
     }
 }
