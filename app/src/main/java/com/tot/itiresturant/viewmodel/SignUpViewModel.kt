53038d894fd987358cd34ef1281e818c160777b7
AbdElwahabZayed <ammz1997@gmail.com>
2019-10-31 18:31:25 +0200

    home update V 2.0

---
+++ app/src/main/java/com/tot/itiresturant/viewmodel/SignUpViewModel.kt
@@ -0,0 +1,10 @@
+package com.tot.itiresturant.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.tot.itiresturant.repo.SignUpRepository
+
+class SignUpViewModel (application: Application) : AndroidViewModel(application) {
+
+    private var repository: SignUpRepository = SignUpRepository(application)
+}
