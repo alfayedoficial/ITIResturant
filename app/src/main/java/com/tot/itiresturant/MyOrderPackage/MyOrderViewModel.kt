48b14021d6230d61a23b3831ada1b0873a60215a
hanan012 <helsaid13@gmail.com>
2019-10-31 15:24:56 +0200

    order class

---
+++ app/src/main/java/com/tot/itiresturant/MyOrderPackage/MyOrderViewModel.kt
@@ -0,0 +1,7 @@
+package com.tot.itiresturant.MyOrderPackage
+
+import androidx.lifecycle.ViewModel
+
+class MyOrderViewModel : ViewModel() {
+
+}
---
+++ app/src/main/java/com/tot/itiresturant/view/fragment/MyOrderFragment.kt
@@ -0,0 +1,34 @@
+package com.tot.itiresturant.view.fragment
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tot.itiresturant.MyOrderPackage.MyOrderViewModel
+
+import com.tot.itiresturant.R
+
+class MyOrderFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MyOrderFragment()
+    }
+
+    private lateinit var viewModel: MyOrderViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.my_order_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MyOrderViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
