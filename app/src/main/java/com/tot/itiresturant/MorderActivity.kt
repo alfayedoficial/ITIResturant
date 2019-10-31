e4e488c93eddb67aa054b04c786cc695377d26c3
alialfayed1 <alialfayed1@gmail.com>
2019-10-31 13:43:14 +0200

    activity

---
+++ app/src/main/java/com/tot/itiresturant/MorderActivity.kt
@@ -0,0 +1,12 @@
+package com.tot.itiresturant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MorderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_morder)
+    }
+}
--- app/src/main/java/com/tot/itiresturant/SignUpActivity.kt
+++ app/src/main/java/com/tot/itiresturant/SignUpActivity.kt
@@ -3,10 +3,10 @@ package com.tot.itiresturant
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
     }
 }
