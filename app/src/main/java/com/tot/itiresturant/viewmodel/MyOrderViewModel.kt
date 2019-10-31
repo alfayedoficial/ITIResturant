80ad9c18446eb2aaf944d3f7b33ac9cdfa6c95d7
Waleed <waleedadel222@gmail.com>
2019-10-31 16:12:28 +0200

    view model in gradle

--- app/src/main/java/com/tot/itiresturant/viewmodel/MyOrderViewModel.kt
+++ app/src/main/java/com/tot/itiresturant/viewmodel/MyOrderViewModel.kt
@@ -1,8 +1,26 @@
 package com.tot.itiresturant.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.tot.itiresturant.model.Order
+import com.tot.itiresturant.view.activity.CartActivity
+import com.tot.itiresturant.view.fragment.MyOrderFragment
 
-class MyOrderViewModel : ViewModel() {
+class MyOrderViewModel() : ViewModel() {
 
+    private var mutableLiveData: MutableLiveData<List<Order>>? = null
+
+    private var cartActivity: CartActivity? = null
+    private var myOrderFragment: MyOrderFragment? = null
+
+    constructor(cartActivity: CartActivity) : this() {
+        mutableLiveData = MutableLiveData<List<Order>>()
+        this.cartActivity = cartActivity
+    }
+
+    constructor(myOrderFragment: MyOrderFragment) : this() {
+        mutableLiveData = MutableLiveData<List<Order>>()
+        this.myOrderFragment = myOrderFragment
+    }
 
 }
