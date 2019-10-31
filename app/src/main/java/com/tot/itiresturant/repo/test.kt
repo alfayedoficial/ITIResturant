e879ea7a2d070a7d42b15e1e3073adaf6bb2528f
alialfayed1 <alialfayed1@gmail.com>
2019-10-31 13:51:37 +0200

    MVVM

---
+++ app/src/main/java/com/tot/itiresturant/repo/test.kt
@@ -0,0 +1,6 @@
+package com.tot.itiresturant.repo
+
+/**
+ * Class do :
+ * Created by Group 4 ITI (Eng/Bassen - Eng Fatma - Eng Ali)
+ */
---
+++ app/src/main/java/com/tot/itiresturant/view/adapter/test.kt
@@ -0,0 +1,6 @@
+package com.tot.itiresturant.view.adapter
+
+/**
+ * Class do :
+ * Created by Group 4 ITI (Eng/Bassen - Eng Fatma - Eng Ali)
+ */
