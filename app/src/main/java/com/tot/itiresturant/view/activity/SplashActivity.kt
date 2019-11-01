23b8dd161af1c95ba389c304ca2d49247d6d3949
alialfayed1 <alialfayed1@gmail.com>
2019-11-01 15:54:56 +0200

    Update design

--- app/src/main/java/com/tot/itiresturant/view/activity/SplashActivity.kt
+++ app/src/main/java/com/tot/itiresturant/view/activity/SplashActivity.kt
@@ -1,13 +1,26 @@
 package com.tot.itiresturant.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.tot.itiresturant.R
 
 class SplashActivity : AppCompatActivity() {
-
+    private val SPLASH_DISPLAY_LENGTH = 2000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_main)
+
+        durationOfWait()
+
+    }
+    private fun durationOfWait() {
+        Handler().postDelayed({
+            /* Create an Intent that will start the Menu-Activity. */
+            val mainIntent = Intent(this, FirstActivity::class.java)
+            startActivity(mainIntent)
+            this.finish()
+        }, SPLASH_DISPLAY_LENGTH.toLong())
     }
 }
